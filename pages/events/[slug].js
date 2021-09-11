9ce3f035075dbc8aa139c3129f33e582fc0c948c
kremsnita420 <duranovic.safet@gmail.com>
2021-09-11 12:33:02 +0200

    commit

--- pages/events/[slug].js
+++
@@ -1,9 +0,0 @@
-
-export default function EventPage() {
-
-    return (
-        <div>
-            <h1>My Event</h1>
-        </div>
-    )
-}
--- pages/events/index.js
+++
@@ -1,7 +0,0 @@
-export default function HomePage() {
-    return (
-        <div>
-            <h1>My Events</h1>
-        </div>
-    )
-}
