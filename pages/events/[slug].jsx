7e50f3b9c1f448b9dc5457e6250540d598e98f87
kremsnita420 <duranovic.safet@gmail.com>
2021-09-12 12:11:30 +0200

    fetch and display eventas data

--- pages/events/[slug].jsx
+++ pages/events/[slug].jsx
@@ -1,9 +1,22 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-export default function EventPage() {
+export default function EventPage(evt) {
+	console.log(evt)
 	return (
 		<Layout>
 			<h1>My Event</h1>
 		</Layout>
 	)
+}
+
+export async function getServerSideProps({ query: { slug } }) {
+	const res = await fetch(`${API_URL}/events?slug=${slug}`)
+	const events = await res.json()
+
+	return {
+		props: {
+			evt: events[0]
+		}
+	}
 }
