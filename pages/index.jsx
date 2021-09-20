eb8753c92f081659e00b2a26177307c96fab1f3d
kremsnita420 <duranovic.safet@gmail.com>
2021-09-20 12:11:48 +0200

    deploying to vercel

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ import { API_URL } from '@/config/index'
 export default function HomePage({ events }) {
 	return (
 		<Layout>
-			<h1>Upcomming Events</h1>
+			<h1>Upcoming Events</h1>
 			{events.length === 0 && <h3>No events to show</h3>}
 
 			{events.map((evt) => (
@@ -14,11 +14,9 @@ export default function HomePage({ events }) {
 			))}
 
 			{events.length > 0 && (
-				<div className='btn-wrapper'>
-					<Link href='/events'>
-						<a className='btn-secondary'>View all events</a>
-					</Link>
-				</div>
+				<Link href='/events'>
+					<a className='btn-secondary'>View All Events</a>
+				</Link>
 			)}
 		</Layout>
 	)
@@ -30,6 +28,6 @@ export async function getStaticProps() {
 
 	return {
 		props: { events },
-		revalidate: 1, //after 1 second
+		revalidate: 1,
 	}
 }
