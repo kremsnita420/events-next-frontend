094b4bc2dff83d504ae9b1b71fb8006022e3b833
kremsnita420 <duranovic.safet@gmail.com>
2021-10-11 21:37:33 +0200

    Nprogres loading bar implement

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import { AuthProvider } from '@/context/AuthContext'
 import '../styles/globals.css'
+import NextNprogress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <NextNprogress
+        color="#FF0000"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={5}
+        showOnShallow={true}
+      />
       <Component {...pageProps} />
     </AuthProvider>
   )
